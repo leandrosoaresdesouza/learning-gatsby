996f49757c03cfab2b66f94f4b5a2506248fa396
Leandro Soares <lsoares@orio.tech>
2020-04-21 20:02:38 -0300

    Added social links

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/leandrosoaresdesouza",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/leandrosoaresdesouza",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/leandrosoaresdesouza",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/leandrosoaresdesouza",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com/leandrosoaresdesouza",
+  },
+]
+
+export default links
